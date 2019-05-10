30dbc11e9045872220d51cddcace80163a7ff93a
redsharei <syarei@live.jp>
2019-05-10 21:51:16 +0900

    pick1

--- app/src/main/java/io/mikankenshi/akizikan/MainActivity.java
+++ app/src/main/java/io/mikankenshi/akizikan/MainActivity.java
@@ -1,15 +1,64 @@
 package io.mikankenshi.akizikan;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+
+
+public class MainActivity extends FragmentActivity
+        implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    private TextView textView1,textView2;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+    }
+
+//Date Picker
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        String str = String.format(Locale.US, "%d/%d/%d",year, monthOfYear+1, dayOfMonth);
+        textView1.setText( str );
+
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePick();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+
+    }
+
+
+//Time Picker
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        String str = String.format(Locale.US, "%d:%d", hourOfDay, minute);
+        textView2.setText( str );
+
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePick();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
     }
 }
+
+//データを保存する
